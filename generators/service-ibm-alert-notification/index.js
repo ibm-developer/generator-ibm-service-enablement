c1dabcd0910c3ae9a619cc651d0812bb02d09782
gconan@utexas.edu <gammel@us.ibm.com>
2017-08-28 17:09:06 -0500

    IDE messing with my tabs/spaces

--- generators/service-ibm-alert-notification/index.js
+++ generators/service-ibm-alert-notification/index.js
@@ -5,19 +5,19 @@ const SERVICE_NAME = "service-ibm-alert-notification";
 const localDevConfig = ['name', 'password', 'url', 'swaggerui'];
 
 module.exports = class extends BaseGenerator {
-    constructor(args, opts) {
-        super(args, opts, SERVICE_NAME, SCAFFOLDER_PROJECT_PROPERTY_NAME, localDevConfig);
-    }
+	constructor(args, opts) {
+		super(args, opts, SERVICE_NAME, SCAFFOLDER_PROJECT_PROPERTY_NAME, localDevConfig);
+	}
 
-    initializing(){
-        return super.initializing();
-    }
+	initializing(){
+		return super.initializing();
+	}
 
-    configuring(){
-        return super.configuring();
-    }
+	configuring(){
+		return super.configuring();
+	}
 
-    writing(){
-        return super.writing();
-    }
+	writing(){
+		return super.writing();
+	}
 }
