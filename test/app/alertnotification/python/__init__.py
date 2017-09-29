65d0efca87e43ccdcbfdcb6c9e7457e55af516d0
Gabe Terrell <Gabe.Terrell@ibm.com>
2017-09-29 14:25:50 -0500

    Fixed tab/spacing issues

--- test/app/alertnotification/python/__init__.py
+++ test/app/alertnotification/python/__init__.py
@@ -3,16 +3,15 @@ def alert():
 	messages = []
 
 	alertMessage = {
-	    'What': 'sample alert',
-	    'Where': 'someserver.ibm.com',
-	    'Severity': 'Critical'
+		'What': 'sample alert',
+		'Where': 'someserver.ibm.com',
+		'Severity': 'Critical'
 	}
 
 	alert = service_manager.get('alert-notification')
 	res = requests.post(alert['url'], auth=(alert['user'], alert['password']), json=alertMessage)
 
 	if res and res.text:
-            print(res.text)
-	    messages.append('alert sent')
+		messages.append('alert sent')
 
-        return jsonify(messages)
+	return jsonify(messages)
