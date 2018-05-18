67250b708ffa6e367af4589a5df02929edffb294
Erin Schnabel <schnabel@us.ibm.com>
2018-05-18 15:58:06 -0400

    fix(secrets): Make kube secrets optional
    Secrets for provisioned services were being added directly to deployment.yaml, making the helm chart
    environment-specific (service keys were unique/specific to public). This fix makes two changes:
    1) generated secrets are optional (helm chart will not fail to deploy)
    2) generated secrets are appended to a bindings.yaml file (if present), rather than to deployment.yaml
    3) use helm template to verify modified helm charts
    
    ibm-developer/generator-ibm-cloud-enablement#275

--- generators/lib/Utils.js
+++ generators/lib/Utils.js
@@ -4,13 +4,13 @@ const logger = Log4js.getLogger("generator-ibm-service-enablement:Utils");
 const readline = require('readline');
 const fs = require('fs');
 
-const REGEX_ENV = /env:/;
+const REGEX_PORT = /^(\s*)- name: PORT/;
 const REGEX_DEPLOY = /- name: Deploy Stage/;
 const REGEX_KUBE	= /kubernetes_cluster:/;
 const REGEX_BASH = /#!\/bin\/bash/;
 
 
-function addServicesEnvToDeploymentYamlAsync(args) {
+function addServicesEnvToHelmChartAsync(args) {
 	return new Promise((resolve, reject) => {
 		let context = args.context;
 		let destinationPath = args.destinationPath;
@@ -19,11 +19,11 @@ function addServicesEnvToDeploymentYamlAsync(args) {
 
 		let hasServices = context.deploymentServicesEnv && context.deploymentServicesEnv.length > 0;
 		if (!hasServices) {
-			logger.info('No services to add to deployment.yaml');
+			logger.info('No services to add');
 			return resolve();
 		}
 
-		// this deployment.yaml file should've been generated in the generator-ibm-cloud-enablement generator
+		// the helm chart should've been generated in the generator-ibm-cloud-enablement generator
 		// for deploy to Kubernetes using Helm chart
 		let chartFolderPath = `${destinationPath}/chart`;
 		if (!fs.existsSync(chartFolderPath)) {
@@ -31,84 +31,122 @@ function addServicesEnvToDeploymentYamlAsync(args) {
 			return resolve();
 		}
 
-		let deploymentFilePath = `${chartFolderPath}/${context.sanitizedAppName}/templates/deployment.yaml`;
-		logger.info(`deployment.yaml path: ${deploymentFilePath}`);
+		let bindingsFilePath = `${chartFolderPath}/${context.sanitizedAppName}/bindings.yaml`;
+		let bindingsFileExists = fs.existsSync(bindingsFilePath);
+		logger.info(`bindings.yaml exists (${bindingsFileExists}) at ${bindingsFilePath}`);
 
+		let deploymentFilePath = `${chartFolderPath}/${context.sanitizedAppName}/templates/deployment.yaml`;
 		let deploymentFileExists = fs.existsSync(deploymentFilePath);
-		if (!deploymentFileExists) {
-			logger.info(`deployment.yaml does not exist at ${deploymentFilePath}, checking /chart directory`);
+		logger.info(`deployment.yaml exists (${deploymentFileExists}) at ${deploymentFilePath}`);
+
+		if ( !bindingsFileExists && ! deploymentFileExists ){
+			logger.info(`Can't find required yaml files, checking /chart directory`);
+
 			// chart could've been created with different name than expected
-			let chartFolders = fs.readdirSync(`${chartFolderPath}`);
 			// there should only be one folder under /chart, but just in case
+			let chartFolders = fs.readdirSync(`${chartFolderPath}`);
 			for (let i = 0; i < chartFolders.length; i++) {
+
+				bindingsFilePath = `${chartFolderPath}/${chartFolders[i]}/bindings.yaml`;
+				bindingsFileExists = fs.existsSync(bindingsFilePath);
+				logger.info(`bindings.yaml exists (${bindingsFileExists}) at ${bindingsFilePath}`);
+
 				deploymentFilePath = `${chartFolderPath}/${chartFolders[i]}/templates/deployment.yaml`;
 				deploymentFileExists = fs.existsSync(deploymentFilePath);
-				if (deploymentFileExists) {
-					logger.info(`found deployment.yaml file at ${deploymentFilePath}`);
+				logger.info(`deployment.yaml exists (${deploymentFileExists}) at ${deploymentFilePath}`);
+
+				if (bindingsFileExists || deploymentFileExists) {
 					break;
 				}
 			}
 		}
 
-		if (!deploymentFileExists) {
+		if ( bindingsFileExists ) {
+			logger.info(`Adding ${context.deploymentServicesEnv.length} to env in bindings.yaml` );
+			return appendBindingsYaml(bindingsFilePath, context.deploymentServicesEnv, resolve, reject);
+		} else if ( deploymentFileExists ) {
+			logger.info(`Adding ${context.deploymentServicesEnv.length} to env in deployment.yaml` );
+			return appendDeploymentYaml(deploymentFilePath, context.deploymentServicesEnv, resolve, reject);
+		} else {
 			logger.error('deployment.yaml not found, cannot add services to env');
 			return resolve();
 		}
+	});
+}
 
-		logger.info(`deployment.yaml exists, adding ${context.deploymentServicesEnv.length} to env` );
+function appendBindingsYaml(bindingsFilePath, services, resolve, reject) {
+	// Bindings file is a straight-up append, with no special indenting required
+	fs.readFile(bindingsFilePath, 'utf-8', (err, data) => {
+		if (err) {
+			return reject(err);
+		}
+		data = data.trim() + '\n' + generateSecretKeyReferences(services, '');
+		//console.log(data);
 
-		let readStream = fs.createReadStream(deploymentFilePath);
-		let promiseIsRejected = false;
-		readStream.on('error', (err) => {
-			logger.error('failed to read deployment.yaml from filesystem: ' + err.message);
-			reject(err);
-			promiseIsRejected = true;
-		});
-		let rl = readline.createInterface({ input: readStream });
-
-		let deploymentFileString = '';
-		rl.on('line', (line) => {
-			deploymentFileString += `${line}\n`;
-
-			let envIndex = line.search(REGEX_ENV);
-			if (envIndex > -1) {
-				logger.info(`deployment.yaml env section index: ${envIndex}`);
-
-				let spacesPrefix = line.slice(0, envIndex);
-
-				// TODO: more robust check of spaces to prefix
-				// valid to have items under env: section be at same level or indented by two spaces
-				// env:
-				// - name: service_watson_tone_analyzer
-				// also valid to have this ->
-				// env:
-				//	 - name: service_watson_tone_analyzer
-				// so check if env: section already has items under it, if so, use same amount of spaces as existing items
-
-				let servicesEnvString = '';
-				context.deploymentServicesEnv.forEach((serviceEntry) => {
-					servicesEnvString +=
-						`${spacesPrefix}  - name: ${serviceEntry.name}\n` +
-						`${spacesPrefix}    valueFrom:\n` +
-						`${spacesPrefix}      secretKeyRef:\n` +
-						`${spacesPrefix}        name: ${serviceEntry.valueFrom.secretKeyRef.name}\n` +
-						`${spacesPrefix}        key: ${serviceEntry.valueFrom.secretKeyRef.key}\n`;
-				});
-				deploymentFileString += servicesEnvString;
+		fs.writeFile(bindingsFilePath, data, (err) => {
+			if (err) {
+				logger.error('failed to write updated bindings.yaml to filesystem: ' + err.message);
+				return reject(err);
+			} else {
+				logger.info('finished updating bindings.yaml and wrote to filesystem');
+				return resolve();
 			}
-		}).on('close', () => {
-			if (promiseIsRejected) { return; }
-			fs.writeFile(deploymentFilePath, deploymentFileString, (err) => {
-				if (err) {
-					logger.error('failed to write updated deployment.yaml to filesystem: ' + err.message);
-					reject(err);
-				} else {
-					logger.info('finished updating deployment.yaml and wrote to filesystem');
-					resolve();
-				}
-			});
 		});
 	});
+}
+
+function appendDeploymentYaml(deploymentFilePath, services, resolve, reject) {
+	let readStream = fs.createReadStream(deploymentFilePath);
+	let promiseIsRejected = false;
+	readStream.on('error', (err) => {
+		logger.error('failed to read deployment.yaml from filesystem: ' + err.message);
+		reject(err);
+		promiseIsRejected = true;
+	});
+
+	let envSection = false;
+	let deploymentFileString = '';
+	let rl = readline.createInterface({ input: readStream });
+	rl.on('line', (line) => {
+
+		envSection |= (line.indexOf('env:') > -1); // did we find env: yet?
+		if (envSection ) { // we found env, look for -name: PORT, will insert above that
+			let match = line.match(REGEX_PORT); // regex, captures leading whitespace
+			if ( match !== null ) {
+				console.log(`Found ${match[0]}`);
+				deploymentFileString += generateSecretKeyReferences(services, `${match[1]}`);
+				envSection = false;               // all done with env section.
+			}
+		}
+
+		// NOW append the line to the string
+		deploymentFileString += `${line}\n`;
+	}).on('close', () => {
+		if (promiseIsRejected) { return; }
+		fs.writeFile(deploymentFilePath, deploymentFileString, (err) => {
+			if (err) {
+				logger.error('failed to write updated deployment.yaml to filesystem: ' + err.message);
+				reject(err);
+			} else {
+				logger.info('finished updating deployment.yaml and wrote to filesystem');
+				resolve();
+			}
+		});
+	});
+}
+
+function generateSecretKeyReferences(services, prefix) {
+	let servicesEnvString = '';
+	services.forEach((serviceEntry) => {
+		servicesEnvString +=
+			`${prefix}- name: ${serviceEntry.name}\n` +
+			`${prefix}  valueFrom:\n` +
+			`${prefix}    secretKeyRef:\n` +
+			`${prefix}      name: ${serviceEntry.valueFrom.secretKeyRef.name}\n` +
+			`${prefix}      key: ${serviceEntry.valueFrom.secretKeyRef.key}\n` +
+			`${prefix}      optional: true\n`;
+	});
+	return servicesEnvString;
 }
 
 function addServicesToPipelineYamlAsync(args) {
@@ -178,6 +216,6 @@ function addServicesToPipelineYamlAsync(args) {
 
 
 module.exports = {
-	addServicesEnvToDeploymentYamlAsync: addServicesEnvToDeploymentYamlAsync,
+	addServicesEnvToHelmChartAsync: addServicesEnvToHelmChartAsync,
 	addServicesToPipelineYamlAsync: addServicesToPipelineYamlAsync
 };
---
+++ test/app-services-with-bindings-test.js
@@ -0,0 +1,240 @@
+const path = require('path');
+const yassert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+
+const Log4js = require('log4js');
+// const difference = require('lodash/difference');
+const includes = require('lodash/includes');
+const yml = require('js-yaml');
+const logger = Log4js.getLogger("generator-ibm-service-enablement:app-generatorbase-test");
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+const optionsBluemix = Object.assign({}, require('./resources/bluemix.json'));
+
+const APP_NAME = 'AcmeProject'.toLowerCase();
+const GENERATOR_NODE_PATH = '../generators/language-node-express/index.js';
+
+const RESOURCES_PATH = path.join(__dirname, './resources');
+
+let serviceNames = [];
+let keyRefNames = [];
+const scaffolderMapping = require('../generators/resources/scaffolderMapping.json');
+
+const generatorFolderNames = fs.readdirSync(path.join(__dirname, '..', 'generators'));
+generatorFolderNames.forEach(folder => {
+	if(folder.startsWith('service-')){
+		serviceNames.push(folder.replace(/-/g,'_'));
+	}
+});
+
+const serviceKeys = Object.keys(scaffolderMapping);
+serviceKeys.forEach(serviceKey => {
+	let scaffolderKey = scaffolderMapping[serviceKey];
+	let service = Array.isArray(optionsBluemix[scaffolderKey]) ? optionsBluemix[scaffolderKey][0] : optionsBluemix[scaffolderKey];
+	if(service.hasOwnProperty('serviceInfo')){
+		keyRefNames.push(`binding-${service.serviceInfo.name}`);
+	}
+});
+
+let assertYmlContent = function(actual, expected, label) {
+	yassert.strictEqual(actual, expected, 'Expected ' + label + ' to be ' + expected + ', found ' + actual);
+}
+
+function testServiceKeys(bindings) {
+	let found = [];
+
+	bindings.forEach(item => {
+		// skip env entries that pre-exist in bindings.yaml
+		if(item.name.indexOf('IBM_') === 0 ||
+			item.name.indexOf('MONGO_') === 0 ||
+			item.name.indexOf('PORT') === 0 ||
+			item.name.indexOf('APPLICATION_NAME') === 0 ) {
+			return;
+		}
+
+		found.push(item.name);
+
+		yassert.ok(item.name, 'name should exist: ' + JSON.stringify(item));
+		yassert.ok(item.valueFrom, 'valueFrom should exist: ' + JSON.stringify(item));
+		yassert.ok(item.valueFrom.secretKeyRef, 'valueFrom.secretKeyRef should exist: ' + JSON.stringify(item));
+		yassert.ok(item.valueFrom.secretKeyRef.optional, 'valueFrom.secretKeyRef.optional should exist: ' + JSON.stringify(item));
+
+		yassert.ok(includes(serviceNames, item.name), '${item.name} should exist within the following available services: ' + JSON.stringify(serviceNames));
+		yassert.ok(includes(keyRefNames, item.valueFrom.secretKeyRef.name),
+			'${item.valueFrom.secretKeyRef.name} should be a valid secretKeyRef within the following test service secrets: ' + JSON.stringify(keyRefNames));
+	});
+
+	// let missing = difference(serviceNames, found);
+	// yassert.ok(missing.length === 0, `not all services found in bindings.yaml: ${missing.toString()}`);
+}
+
+function createFiles(dir, name, useBindings) {
+	fs.mkdirSync(path.join(dir, `/chart`));
+	fs.mkdirSync(path.join(dir, `/chart/${name}`));
+	fs.mkdirSync(path.join(dir, `/chart/${name}/templates`));
+
+	if ( useBindings ) {
+		fs.createReadStream(path.join(RESOURCES_PATH, `bindings.yaml`)).pipe(fs.createWriteStream(path.join(dir, `/chart/${name}/bindings.yaml`)));
+		fs.createReadStream(path.join(RESOURCES_PATH, `deployment.yaml`)).pipe(fs.createWriteStream(path.join(dir, `/chart/${name}/templates/deployment.yaml`)));
+	} else {
+		fs.createReadStream(path.join(RESOURCES_PATH, `deployment-nobindings.yaml`)).pipe(fs.createWriteStream(path.join(dir, `/chart/${name}/templates/deployment.yaml`)));
+	}
+	fs.createReadStream(path.join(RESOURCES_PATH, `values.yaml`)).pipe(fs.createWriteStream(path.join(dir, `/chart/${name}/values.yaml`)));
+	fs.createReadStream(path.join(RESOURCES_PATH, `Chart.yaml`)).pipe(fs.createWriteStream(path.join(dir, `/chart/${name}/Chart.yaml`)));
+	fs.createReadStream(path.join(RESOURCES_PATH, `deployment.yaml`)).pipe(fs.createWriteStream(path.join(dir, `/chart/${name}/templates/deployment.yaml`)));
+	fs.createReadStream(path.join(RESOURCES_PATH, `service.yaml`)).pipe(fs.createWriteStream(path.join(dir, `/chart/${name}/templates/service.yaml`)));
+}
+
+function verifyHelmChart(chartLocation) {
+	it(`appending services to ${chartLocation} env results in a valid helm chart`, (done) => {
+		exec('helm', (error, stdout, stderr) => {
+			if (error) {
+				console.log(stderr);
+				// helm not available. Fail over to raw parsing of bindings.yaml
+
+				const rawbindingsyml = fs.readFileSync(path.join(chartLocation, `bindings.yaml`), 'utf-8');
+				// escape double quotes and comment out helm conditionals so it can be parsed by js-yaml
+				const newbindingsyml = rawbindingsyml.replace('"+" "_"', '\\"+\\" \\"_\\"');
+				testServiceKeys(yml.safeLoad(newbindingsyml));
+				done();
+			} else {
+				exec('helm template ' + chartLocation + '/', {maxBuffer: 20 * 1024 * 1024}, (error, stdout, stderr) => {
+					if ( error ) {
+						console.log(stdout);
+						done(new Error(stderr))
+					} else {
+						// Uncomment to view locally rendered helm charts
+						// console.log(stdout);
+						// template command will render two charts: service and Deployment
+						let charts = yml.safeLoadAll(stdout);
+						assertYmlContent(charts[1].kind, 'Deployment', 'charts[1].kind');
+						testServiceKeys(charts[1].spec.template.spec.containers[0].env);
+						done();
+					}
+				});
+			}
+		});
+	});
+}
+
+describe('app-services-with-bindings', function () {
+	this.timeout(10 * 1000); // 10 seconds, Travis might be slow
+
+	let lang = 'NODE';
+	let generatorPath = GENERATOR_NODE_PATH;
+
+	describe(`app-services-with-bindings ${lang}`, () => {
+		let context = {};
+		before(() => {
+
+			context.bluemix = JSON.parse(JSON.stringify(optionsBluemix));
+			context.loggerLevel = logger.level;
+			context.sanitizedAppName = context.bluemix.name.toLowerCase();
+
+			context.bluemix.backendPlatform = lang;
+			context.language = context.bluemix.backendPlatform.toLowerCase();
+
+			return helpers
+				.run(path.join(__dirname, generatorPath))
+				.inTmpDir((dir) => {
+					// `dir` is the path to the new temporary directory
+					createFiles(dir, APP_NAME, true);
+				})
+				.withOptions({
+					context: context
+				})
+				.then((tmpDir) => {
+					console.info("tmpDir", tmpDir);
+				});
+		});
+
+		verifyHelmChart(path.join('.', `chart/${APP_NAME}`));
+	});
+
+	describe(`app-services-with-bindings ${lang}, chart folder different from app name`, () => {
+		const chartFolderName = 'mychart';
+
+		before(() => {
+			let context = {};
+			context.bluemix = JSON.parse(JSON.stringify(optionsBluemix));
+			context.loggerLevel = logger.level;
+			context.sanitizedAppName = context.bluemix.name.toLowerCase();
+
+			context.bluemix.backendPlatform = lang;
+			context.language = context.bluemix.backendPlatform.toLowerCase();
+
+			return helpers
+				.run(path.join(__dirname, generatorPath))
+				.inTmpDir((dir) => {
+					// `dir` is the path to the new temporary directory
+					createFiles(dir, chartFolderName, true);
+				})
+				.withOptions({
+					context: context
+				})
+				.then((tmpDir) => {
+					console.info("tmpDir", tmpDir);
+				});
+		});
+
+		verifyHelmChart(path.join('.', `chart/${chartFolderName}`));
+	});
+
+	describe(`app-services-without-bindings ${lang}`, () => {
+		let context = {};
+		before(() => {
+
+			context.bluemix = JSON.parse(JSON.stringify(optionsBluemix));
+			context.loggerLevel = logger.level;
+			context.sanitizedAppName = context.bluemix.name.toLowerCase();
+
+			context.bluemix.backendPlatform = lang;
+			context.language = context.bluemix.backendPlatform.toLowerCase();
+
+			return helpers
+				.run(path.join(__dirname, generatorPath))
+				.inTmpDir((dir) => {
+					// `dir` is the path to the new temporary directory
+					createFiles(dir, APP_NAME, false);
+				})
+				.withOptions({
+					context: context
+				})
+				.then((tmpDir) => {
+					console.info("tmpDir", tmpDir);
+				});
+		});
+
+		verifyHelmChart(path.join('.', `chart/${APP_NAME}`));
+	});
+
+	describe(`app-services-without-bindings ${lang}, chart folder different from app name`, () => {
+		const chartFolderName = 'mychart';
+
+		before(() => {
+			let context = {};
+			context.bluemix = JSON.parse(JSON.stringify(optionsBluemix));
+			context.loggerLevel = logger.level;
+			context.sanitizedAppName = context.bluemix.name.toLowerCase();
+
+			context.bluemix.backendPlatform = lang;
+			context.language = context.bluemix.backendPlatform.toLowerCase();
+
+			return helpers
+				.run(path.join(__dirname, generatorPath))
+				.inTmpDir((dir) => {
+					// `dir` is the path to the new temporary directory
+					createFiles(dir, chartFolderName, false);
+				})
+				.withOptions({
+					context: context
+				})
+				.then((tmpDir) => {
+					console.info("tmpDir", tmpDir);
+				});
+		});
+
+		verifyHelmChart(path.join('.', `chart/${chartFolderName}`));
+	});
+});
