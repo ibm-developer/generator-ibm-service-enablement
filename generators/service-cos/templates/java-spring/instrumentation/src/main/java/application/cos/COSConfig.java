b73e8eff3b8e2e00614ed68e29302580061cf1bf
Quan Vo <qvo1987@gmail.com>
2018-06-18 15:04:54 -0500

    add index.js

--- generators/service-cos/templates/java-spring/instrumentation/src/main/java/application/cos/COSConfig.java
+++ generators/service-cos/templates/java-spring/instrumentation/src/main/java/application/cos/COSConfig.java
@@ -1,4 +1,4 @@
-package application.objectstorage;
+package application.cos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
