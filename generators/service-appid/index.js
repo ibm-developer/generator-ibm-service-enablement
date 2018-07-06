6ca48f15582300d827f7907425f1f570b73732bd
Aerex <nanaus13@yahoo.com>
2018-07-06 17:07:17 -0500

    fix: create mapping and localdev-config even when sdk is not available
    patch includes removing deprecated auth instead use appid

--- generators/service-appid/index.js
+++ generators/service-appid/index.js
@@ -1,6 +1,6 @@
 'use strict';
 const BaseGenerator = require('../lib/generatorbase');
-const SCAFFOLDER_PROJECT_PROPERTY_NAME = "auth";
+const SCAFFOLDER_PROJECT_PROPERTY_NAME = "appid";
 const CLOUD_FOUNDRY_SERVICE_NAME = "AppID";
 const CUSTOM_SERVICE_KEY = 'appid';
 const CUSTOM_CRED_KEYS = ['tenant_id', 'oauth_server_url', 'profiles_url', 'secret', 'client_id']
