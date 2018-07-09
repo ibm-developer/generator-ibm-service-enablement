a6f92d7d7dca27f331ee1a46ed86694b22a8fc7f
Audrey <audreylemberger@ibm.com>
2018-07-09 10:58:55 -0500

    fix: fixed environment variables

--- generators/service-cloud-object-storage/index.js
+++ generators/service-cloud-object-storage/index.js
@@ -1,9 +1,10 @@
 'use strict';
+
 const BaseGenerator = require('../lib/generatorbase');
 const SCAFFOLDER_PROJECT_PROPERTY_NAME = 'cloudobjectstorage';
-const CLOUD_FOUNDRY_SERVICE_NAME = '8106946b-99ba-493a-929e-0c97ead623d3';
-const CUSTOM_SERVICE_KEY = 'cos';
-const CUSTOM_CRED_KEYS = ['apikey', 'endpoints', 'iam_apikey_description', 'iam_apikey_name', 'iam_role_crn', 'iam_serviceid_crn', 'resource_instance_id'];
+const CLOUD_FOUNDRY_SERVICE_NAME = 'cloud-object-storage';
+const CUSTOM_SERVICE_KEY = 'cloud-object-storage';
+
 const config = {
 	cloudFoundryIsArray: false,
 	mappingVersion: 1
@@ -11,18 +12,18 @@ const config = {
 
 module.exports = class extends BaseGenerator {
 	constructor(args, opts) {
-		super(args, opts, SCAFFOLDER_PROJECT_PROPERTY_NAME, CLOUD_FOUNDRY_SERVICE_NAME, CUSTOM_SERVICE_KEY, CUSTOM_CRED_KEYS);
+		super(args, opts, SCAFFOLDER_PROJECT_PROPERTY_NAME, CLOUD_FOUNDRY_SERVICE_NAME, CUSTOM_SERVICE_KEY)
 	}
 
 	initializing() {
-		return super.initializing();
+		return super.initializing()
 	}
 
 	configuring() {
-		return super.configuring(config);
+		return super.configuring(config)
 	}
 
 	writing() {
-		return super.writing();
+		return super.writing()
 	}
 };
