b1de1c9c5d9f00b38c6ab7ec2e9c002e45fdd213
Audrey <audreylemberger@ibm.com>
2018-07-09 11:40:28 -0500

    fix: fixed indentation in tests

--- test/app/cloud-object-storage/python/__init__.py
+++ test/app/cloud-object-storage/python/__init__.py
@@ -3,20 +3,20 @@ os = None
 def testCloudObjectStorage():
 	new_bucket = 'NewBucket'
 	messages = []
+	workspace_id = '7fa12afa-a4b0-4646-8510-deda95f4a640'
+	cos = service_manager.get('cloud-object-storage')
 
 	# workspace_id is not a supplied credential from service_manager,
 	# as each conversation service can have several workspaces.
 	# thus exposing the credential, while not ideal, in this case is okay
-    workspace_id = '7fa12afa-a4b0-4646-8510-deda95f4a640'
-    cos = service_manager.get('cloud-object-storage')
 
-    messages.append('test container was created')
-    try:
-        os.get_container('test')
-        cos.create_bucket(Bucket=new_bucket)
-        messages.append('bucket was created')
-    except Exception as e:
-        os.get_container('test')
-        cos.create_bucket(Bucket=new_bucket)
-        messages.append('bucket was created')
-    return jsonify(messages)
+	messages.append('test container was created')
+	try:
+		os.get_container('test')
+		cos.create_bucket(Bucket=new_bucket)
+		messages.append('bucket was created')
+	except Exception as e:
+		os.get_container('test')
+		cos.create_bucket(Bucket=new_bucket)
+		messages.append('bucket was created')
+	return jsonify(messages)
