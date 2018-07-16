ab6dea50a0838f1c519fd62045c0004c2bde1402
jkingoliver <king@us.ibm.com>
2018-07-16 14:37:22 -0500

    fix: update apikey for VR in python

--- generators/service-watson-visual-recognition/templates/python/instrumentation.py
+++ generators/service-watson-visual-recognition/templates/python/instrumentation.py
@@ -1,11 +1,11 @@
 from ibmcloudenv import IBMCloudEnv
 from watson_developer_cloud import VisualRecognitionV3
 
-if IBMCloudEnv.getString('watson_visual_recognition_api_key'):
+if IBMCloudEnv.getString('watson_visual_recognition_apikey'):
     iam_url = 'https://iam.stage1.bluemix.net/identity/token' if 'staging' in IBMCloudEnv.getString('watson_visual_recognition_url') else 'https://iam.bluemix.net/identity/token'
     visual_recognition = VisualRecognitionV3(
         url=IBMCloudEnv.getString('watson_visual_recognition_url'),
-        iam_api_key=IBMCloudEnv.getString('watson_visual_recognition_api_key'),
+        iam_api_key=IBMCloudEnv.getString('watson_visual_recognition_apikey'),
         version='2018-03-19',
         iam_url=iam_url)
 else:
