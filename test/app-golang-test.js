5164cd0c9ab2b2b51cf42aebfb0a1d71f1f48fdc
Michael Blume <mablume97@gmail.com>
2018-07-31 08:20:11 -0500

    chore: remove test for services Go doesn't support

--- test/app-golang-test.js
+++ test/app-golang-test.js
@@ -25,7 +25,7 @@ describe('golang-gin', function () {
 				console.info("tmpDir", tmpDir);
 			});
 	});
-
+	
 	it('Can run successful generation and create files', () => {
 		yassert.file('server');
 		yassert.file('server/config');
@@ -33,102 +33,7 @@ describe('golang-gin', function () {
 		yassert.file(SERVER_LOCALDEV_CONFIG_JSON);
 		yassert.file('services/services.go');
 	});
-
-	// it('Can add Apache Spark instrumentation', () => {
-	// 	testAll('apache-spark', {
-	// 		apache_spark_cluster_master_url: optionsBluemix.apacheSpark.cluster_master_url,
-	// 		apache_spark_tenant_id: optionsBluemix.apacheSpark.tenant_id,
-	// 		apache_spark_tenant_secret: optionsBluemix.apacheSpark.tenant_secret
-	// 	});
-	// });
-
-	// it('Can add AppID/Auth instrumentation', () => {
-	// 	testAll('appid', {
-	// 		appid_tenantId: optionsBluemix.auth.tenantId,
-	// 		appid_clientId: optionsBluemix.auth.clientId,
-	// 		appid_secret: optionsBluemix.auth.secret,
-	// 		appid_oauthServerUrl: optionsBluemix.auth.oauthServerUrl,
-	// 		appid_profilesUrl: optionsBluemix.auth.profilesUrl
-	// 	});
-	// });
-
-	// it('Can add Cloudant instrumentation', () => {
-	// 	testAll('cloudant', {
-	// 		cloudant_username: optionsBluemix.cloudant[0].username,
-	// 		cloudant_password: optionsBluemix.cloudant[0].password,
-	// 		cloudant_url: optionsBluemix.cloudant[0].url
-	// 	});
-	// });
-
-	// it('Can add ObjectStorage instrumentation', () => {
-	// 	testAll('object-storage', {
-	// 		object_storage_projectId: optionsBluemix.objectStorage[0].projectId,
-	// 		object_storage_userId: optionsBluemix.objectStorage[0].userId,
-	// 		object_storage_password: optionsBluemix.objectStorage[0].password,
-	// 		object_storage_region: optionsBluemix.objectStorage[0].region
-	// 	});
-	// });
-
-	// it('Can add DashDB instrumentation', () => {
-	// 	testAll('dashdb', {
-	// 		dashdb_dsn: optionsBluemix.dashDb.dsn,
-	// 		dashdb_jdbcurl: optionsBluemix.dashDb.jdbcurl
-	// 	});
-	// });
-
-	// it('Can add DB2 instrumentation', () => {
-	// 	testAll('db2', {
-	// 		db2_dsn: optionsBluemix.db2OnCloud.dsn,
-	// 		db2_ssljdbcurl: optionsBluemix.db2OnCloud.ssljdbcurl
-	// 	});
-	// });
-
-	// it('Can add Finance - Historical Instrument Analytics instrumentation', () => {
-	// 	testAll('finance-simulated-historical-instrument-analytics', {
-	// 		finance_historical_instrument_analytics_uri: optionsBluemix.historicalInstrumentAnalytics.uri,
-	// 		finance_historical_instrument_analytics_accessToken: optionsBluemix.historicalInstrumentAnalytics.accessToken
-	// 	});
-
-	// });
-
-	// it('Can add Finance - Instrument Analytics instrumentation', () => {
-	// 	testAll('finance-instrument-analytics', {
-	// 		finance_instrument_analytics_uri: optionsBluemix.instrumentAnalytics.uri,
-	// 		finance_instrument_analytics_accessToken: optionsBluemix.instrumentAnalytics.accessToken
-	// 	});
-	// });
-
-	// it('Can add Finance - Investment Portfolio instrumentation', () => {
-	// 	testAll('finance-investment-portfolio', {
-	// 		finance_investment_portfolio_url: optionsBluemix.investmentPortfolio.url,
-	// 		finance_investment_portfolio_writer_userid: optionsBluemix.investmentPortfolio.writer.userid,
-	// 		finance_investment_portfolio_writer_password: optionsBluemix.investmentPortfolio.writer.password,
-	// 		finance_investment_portfolio_reader_userid: optionsBluemix.investmentPortfolio.reader.userid,
-	// 		finance_investment_portfolio_reader_password: optionsBluemix.investmentPortfolio.reader.password
-	// 	});
-	// });
-
-	// it('Can add Finance - Predictive Market Scenarios instrumentation', () => {
-	// 	testAll('finance-predictive-market-scenarios', {
-	// 		finance_predictive_market_scenarios_uri: optionsBluemix.predictiveMarketScenarios.uri,
-	// 		finance_predictive_market_scenarios_accessToken: optionsBluemix.predictiveMarketScenarios.accessToken
-	// 	});
-	// });
-
-	// it('Can add Finance - Simulated Historical Instrument Analytics instrumentation', () => {
-	// 	testAll('finance-historical-instrument-analytics', {
-	// 		finance_simulated_historical_instrument_analytics_uri: optionsBluemix.simulatedHistoricalInstrumentAnalytics.uri,
-	// 		finance_simulated_historical_instrument_analytics_accessToken: optionsBluemix.simulatedHistoricalInstrumentAnalytics.accessToken
-	// 	});
-	// });
-
-	// it('Can add Finance - Simulated Instrument Analytics instrumentation', () => {
-	// 	testAll('finance-simulated-instrument-analytics', {
-	// 		finance_simulated_instrument_analytics_uri: optionsBluemix.simulatedInstrumentAnalytics.uri,
-	// 		finance_simulated_instrument_analytics_accessToken: optionsBluemix.simulatedInstrumentAnalytics.accessToken
-	// 	});
-	// });
-
+	
 	it('Can add Watson - Assistant instrumentation', () => {
 		testAll('watson-assistant', {
 			watson_assistant_url: optionsBluemix.conversation.url,
@@ -208,51 +113,7 @@ describe('golang-gin', function () {
 		});
 	});
 
-	// it('Can add Weather Insights  instrumentation', () => {
-	// 	testAll('weather-company-data', {
-	// 		weather_company_data_url: optionsBluemix.weatherInsights.url,
-	// 		weather_company_data_username: optionsBluemix.weatherInsights.username,
-	// 		weather_company_data_password: optionsBluemix.weatherInsights.password
-	// 	});
-	// });
-
-	// it('Can add Push instrumentation', () => {
-	// 	testAll('push', {
-	// 		push_appGuid: optionsBluemix.push.appGuid,
-	// 		push_appSecret: optionsBluemix.push.appSecret,
-	// 		push_clientSecret: optionsBluemix.push.clientSecret
-	// 	});
-
-	// });
-
-	// it('Can add AlertNotification instrumentation', () => {
-	// 	testAll('alert-notification', {
-	// 		alert_notification_url: optionsBluemix.alertNotification.url,
-	// 		alert_notification_name: optionsBluemix.alertNotification.name,
-	// 		alert_notification_password: optionsBluemix.alertNotification.password
-	// 	});
-	// });
-
-
-	// it('Can add MongoDB instrumentation', () => {
-	// 	testAll('mongodb', {
-	// 		mongodb_uri: optionsBluemix.mongodb.uri
-	// 	});
-	// });
-
-	// it('Can add Redis instrumentation', () => {
-	// 	testAll('redis', {
-	// 		redis_uri: optionsBluemix.redis.uri
-	// 	});
-	// });
-
-	// it('Can add Postgre instrumentation', () => {
-	// 	testAll('postgre', {
-	// 		postgre_uri: optionsBluemix.postgresql.uri
-	// 	});
-	// });
-
-
+	
 	it('Can run generation with no services', (done) => {
 		for (let key in optionsBluemix) {
 			if (key !== 'name' && key !== 'backendPlatform' && key !== 'server') {
@@ -268,10 +129,6 @@ describe('golang-gin', function () {
 			.then((tmpDir) => {
 				console.info(tmpDir);
 
-				// Gopkg.toml doesn't have any SDKs
-				// yassert.noFileContent(GOPKG_TOML, 'appid');
-				// yassert.noFileContent(GOPKG_TOML, 'cloudant');
-				// yassert.noFileContent(GOPKG_TOML, 'dashdb');
 				yassert.noFileContent(GOPKG_TOML, 'watson-developer-cloud');
 				yassert.noFile('services/services.go');
 				yassert.noFile(SERVER_LOCALDEV_CONFIG_JSON);
