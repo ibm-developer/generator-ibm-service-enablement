3a0e06eada0641bfc6f45e0745634ea15202550e
German Attanasio <germanattanasio@gmail.com>
2018-08-24 13:36:11 -0400

    fix: Update java-spring to use ibm-cloud-spring-boot-service-bind

--- generators/language-java/index.js
+++ generators/language-java/index.js
@@ -13,21 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 'use strict';
 const logger = require('log4js').getLogger("generator-ibm-service-enablement:language-java");
 const Generator = require('yeoman-generator');
 const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
-const PATH_METAINF = "src/main/resources/META-INF/";
+const PATH_METAINF = 'src/main/resources/META-INF/';
 const scaffolderMapping = require('../resources/scaffolderMapping.json');
 
 const Utils = require('../lib/Utils');
 const javaUtils = require('../lib/javautils');
 
-const PATH_MAPPINGS_FILE = "./src/main/resources/mappings.json";
-const PATH_LOCALDEV_FILE = "./src/main/resources/localdev-config.json";
-const TEMPLATE_EXT = ".template";
+const PATH_MAPPINGS_FILE = './src/main/resources/mappings.json';
+const PATH_LOCALDEV_FILE = './src/main/resources/localdev-config.json';
+const TEMPLATE_EXT = '.template';
 const GENERATOR_LOCATION = 'server';
 
 
@@ -37,7 +38,7 @@ module.exports = class extends Generator {
 		super(args, opts);
 		this.context = opts.context;
 		logger.setLevel(this.context.loggerLevel);
-		logger.debug("Constructing");
+		logger.debug('Constructing');
 	}
 
 	//setup all the values we need to pass in the context
@@ -45,8 +46,8 @@ module.exports = class extends Generator {
 		let serviceCredentials,
 			scaffolderKey,
 			serviceKey;
-		this.context.dependenciesFile = "config.json.template";
-		this.context.languageFileExt = "";
+		this.context.dependenciesFile = 'config.json.template';
+		this.context.languageFileExt = '';
 		this.context.generatorLocation = GENERATOR_LOCATION;
 		this.context.addDependencies = this._addDependencies.bind(this);
 		this.context.addMappings = this._addMappings.bind(this);
@@ -66,7 +67,7 @@ module.exports = class extends Generator {
 				scaffolderKey = scaffolderMapping[serviceKey];
 				serviceCredentials = Array.isArray(this.context.bluemix[scaffolderKey])
 					? this.context.bluemix[scaffolderKey][0] : this.context.bluemix[scaffolderKey];
-				logger.debug("Composing with service : " + folder);
+				logger.debug('Composing with service : ' + folder);
 				try {
 					this.context.cloudLabel = serviceCredentials && serviceCredentials.serviceInfo && serviceCredentials.serviceInfo.cloudLabel;
 					this.composeWith(path.join(root, folder), {context: this.context});
@@ -80,10 +81,10 @@ module.exports = class extends Generator {
 
 	writing() {
 		if (this.context.instrumentationAdded) {
-			this._writeFiles(this.context.language + "/**", this.conf);
+			this._writeFiles(this.context.language + '/**', this.conf);
 			this.context.srcFolders.forEach(folder => {
 				if (fs.existsSync(folder)) {
-					this._writeFiles(folder + "/**", this.conf)
+					this._writeFiles(folder + '/**', this.conf)
 				}
 			})
 		}
@@ -107,7 +108,7 @@ module.exports = class extends Generator {
 
 
 	_addDependencies(serviceDependenciesString) {
-		logger.debug("Adding dependencies", serviceDependenciesString);
+		logger.debug('Adding dependencies', serviceDependenciesString);
 		this._processDependencyMetainf(serviceDependenciesString);
 		this.context._addDependencies(serviceDependenciesString);
 	}
@@ -125,7 +126,7 @@ module.exports = class extends Generator {
 	}
 
 	_addLocalDevConfig(devconf) {
-		logger.debug("Adding devconf", devconf);
+		logger.debug('Adding devconf', devconf);
 		if (this.context.bluemix) {
 			let localDevFilePath = this.destinationPath(PATH_LOCALDEV_FILE);
 			this.fs.extendJSON(localDevFilePath, devconf);
@@ -143,7 +144,7 @@ module.exports = class extends Generator {
 	}
 
 	_addCoreDependencies() {
-		let dependenciesString = this.fs.read(this.templatePath() + "/" + this.context.language + "/" + this.context.dependenciesFile);
+		let dependenciesString = this.fs.read(`${this.templatePath()}/${this.context.language}/${this.context.dependenciesFile}`);
 		let template = handlebars.compile(dependenciesString);
 		dependenciesString = template(this.context);
 		this._processDependencyMetainf(dependenciesString);
@@ -153,18 +154,22 @@ module.exports = class extends Generator {
 	_addReadMe(options) {
 		this.fs.copy(
 			options.sourceFilePath,
-			this.destinationPath() + "/docs/services/" + options.targetFileName
+			`${this.destinationPath()}/docs/services/${options.targetFileName}`
 		);
 	}
 
 	_writeFiles(templatePath, data) {
 		//do not write out any files that are marked as processing templates
-		this.fs.copy([this.templatePath(templatePath), '!**/*.template'], this.destinationPath(), {
-			process: function (contents) {
-				let compiledTemplate = handlebars.compile(contents.toString());
-				return compiledTemplate(data);
-			}
-		});
+		try {
+			this.fs.copy([this.templatePath(templatePath), '!**/*.template'], this.destinationPath(), {
+				process: function (contents) {
+					let compiledTemplate = handlebars.compile(contents.toString());
+					return compiledTemplate(data);
+				}
+			});
+		} catch(e) {
+			logger.warn(`No files to copy from ${this.templatePath(templatePath)}`);
+		}
 	}
 
 	end() {
--- generators/service-watson-conversation/templates/node/instrumentation.js
+++ generators/service-watson-conversation/templates/node/instrumentation.js
@@ -3,12 +3,12 @@ const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 
 module.exports = function(app, serviceManager){
 	const assistant = new AssistantV1({
-    iam_apikey: IBMCloudEnv.getString('watson_conversation_apikey'),
-    username: IBMCloudEnv.getString('watson_conversation_username'),
-    password: IBMCloudEnv.getString('watson_conversation_password'),
-    url: IBMCloudEnv.getString('watson_conversation_url'),
-    version: '2018-07-10',
-  });
+		iam_apikey: IBMCloudEnv.getString('watson_conversation_apikey'),
+		username: IBMCloudEnv.getString('watson_conversation_username'),
+		password: IBMCloudEnv.getString('watson_conversation_password'),
+		url: IBMCloudEnv.getString('watson_conversation_url'),
+		version: '2018-07-10',
+	});
 
 	serviceManager.set('watson-conversation', assistant);
 };
