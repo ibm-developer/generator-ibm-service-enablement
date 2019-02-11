c00b760f2a25f52b52818662643eee5de5af79a2
Audrey <audreylemberger@ibm.com>
2019-02-11 15:32:34 -0600

    fix: fixed instrumentation code for watson nlp and text to speech

--- generators/service-watson-text-to-speech/templates/python/instrumentation.py
+++ generators/service-watson-text-to-speech/templates/python/instrumentation.py
@@ -1,11 +1,11 @@
 from ibmcloudenv import IBMCloudEnv
 from watson_developer_cloud import TextToSpeechV1
 
-if IBMCloudEnv.getString('watson-text-to-speech_apikey'):
-    iam_url = 'https://iam.stage1.bluemix.net/identity/token' if 'staging' in IBMCloudEnv.getString('watson_text-to-speech_iam_serviceid_crn') else 'https://iam.bluemix.net/identity/token'
+if IBMCloudEnv.getString('watson_text_to_speech_apikey'):
+    iam_url = 'https://iam.stage1.bluemix.net/identity/token' if 'staging' in IBMCloudEnv.getString('watson_text_to_speech_iam_role_crn') else 'https://iam.bluemix.net/identity/token'
     text_to_speech = TextToSpeechV1(
         url=IBMCloudEnv.getString('watson_text_to_speech_url'),
-        iam_api_key=IBMCloudEnv.getString('watson-text-to-speech_apikey'),
+        iam_api_key=IBMCloudEnv.getString('watson_text_to_speech_apikey'),
         iam_url=iam_url)
 else:
     text_to_speech = TextToSpeechV1(
